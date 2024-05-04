8b3480b026c6ffc22f024cf69ea5647d8ebc4ba6
Hamed <48640037+berserkhmdvhb@users.noreply.github.com>
2024-05-04 09:47:04 +0200

    Add files via upload

---
+++ Projects/Customer/SSIS/repo/Queries/ClearAllTables.sql
@@ -0,0 +1,6 @@
+USE CustomerDWH;
+DELETE FROM dbo.DimCountry
+DELETE FROM dbo.DimProduct
+DELETE FROM dbo.DimSalesPerson
+DELETE FROM dbo.DimState
+TRUNCATE TABLE dbo.FactCustomer
---
+++ Projects/Customer/SSIS/repo/Queries/TableCreation_DimCountry.sql
@@ -0,0 +1,21 @@
+USE [CustomerDWH]
+GO
+
+/****** Object:  Table [dbo].[DimCountry]    Script Date: 5/3/2024 3:03:14 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[DimCountry](
+	[CountryID] [int] NOT NULL,
+	[CountryName] [nvarchar](50) NULL,
+ CONSTRAINT [PK_DimCountry] PRIMARY KEY CLUSTERED 
+(
+	[CountryID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
