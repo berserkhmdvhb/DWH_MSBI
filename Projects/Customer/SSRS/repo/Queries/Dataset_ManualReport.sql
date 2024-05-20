1ce67366bacce08d9244a87f0abc13c9b9fe4bae
Hamed <48640037+berserkhmdvhb@users.noreply.github.com>
2024-05-20 16:30:12 +0200

    Create Dataset_ManualReport.sql

---
+++ Projects/Customer/SSRS/repo/Queries/Dataset_ManualReport.sql
@@ -0,0 +1,15 @@
+SELECT 
+  FactCustomer.CustomerCode,
+	FactCustomer.CustomerName,
+	FactCustomer.CustomerAmount,
+	DimState.StateName,
+	DimSalesPerson.SalesPersonName,
+	DimProduct.ProductName,
+	DimCountry.CountryName,
+	DimDate.[date]
+FROM FactCustomer
+INNER JOIN DimDate ON FactCustomer.SalesDateID_FK = DimDate.date_key
+INNER JOIN DimSalesPerson ON FactCustomer.SalesPersonID_FK = DimSalesPerson.SalesPersonID
+INNER JOIN DimState ON FactCustomer.StateID_FK = DimState.StateID
+INNER JOIN DimCountry ON FactCustomer.CountryID_FK = DimCountry.CountryID
+INNER JOIN DimProduct ON FactCustomer.ProductID_FK = DimProduct.ProductID
