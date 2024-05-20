76db474392222e8443114097f1e4f7bd4d8dc15d
Hamed <48640037+berserkhmdvhb@users.noreply.github.com>
2024-05-20 22:27:47 +0200

    Update DataSource_CustomerDWH_Tab.sql

--- Projects/Customer/SSRS/repo/Queries/Tab/DataSource_CustomerDWH_Tab.sql
+++ Projects/Customer/SSRS/repo/Queries/Tab/DataSource_CustomerDWH_Tab.sql
@@ -11,9 +11,13 @@ FROM FactCustomer
 INNER JOIN DimDate ON FactCustomer.SalesDateID_FK = DimDate.date_key
 INNER JOIN DimSalesPerson ON FactCustomer.SalesPersonID_FK = DimSalesPerson.SalesPersonID
 INNER JOIN DimState ON FactCustomer.StateID_FK = DimState.StateID
-INNER JOIN DimCountry ON FactCustomer.CountryID_FK = DimCountry.CountryID
+INNER JOIN DimCountry ON DimState.CountryID = DimCountry.CountryID -- Ensure correct join here
 INNER JOIN DimProduct ON FactCustomer.ProductID_FK = DimProduct.ProductID
 WHERE 
 	(DimCountry.IsNew = 1)
 	AND 
 	(DimProduct.Ex_Date IS NULL)
+	AND 
+	DimCountry.CountryID = @CountryParam
+	AND 
+	DimState.StateID = @StateParam
