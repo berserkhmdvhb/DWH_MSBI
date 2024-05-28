9208d1b402cd58821d310d62dc82e2a3ee8d318f
Hamed <48640037+berserkhmdvhb@users.noreply.github.com>
2024-05-28 14:02:57 +0200

    Update TableCreation_DimSalesPerson.sql

--- Projects/Customer/SSIS/repo/Queries/TableCreation_DimSalesPerson.sql
+++ Projects/Customer/SSIS/repo/Queries/TableCreation_DimSalesPerson.sql
@@ -1,7 +1,7 @@
 USE [CustomerDWH]
 GO
 
-/****** Object:  Table [dbo].[DimSalesPerson]    Script Date: 5/3/2024 3:03:25 PM ******/
+/****** Object:  Table [dbo].[DimSalesPerson]    Script Date: 5/28/2024 1:57:47 PM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -11,11 +11,10 @@ GO
 CREATE TABLE [dbo].[DimSalesPerson](
 	[SalesPersonID] [int] NOT NULL,
 	[SalesPersonName] [nvarchar](50) NULL,
+	[SalesPersonBossID_FK] [int]
  CONSTRAINT [PK_DimSalesPerson] PRIMARY KEY CLUSTERED 
 (
 	[SalesPersonID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-
-
