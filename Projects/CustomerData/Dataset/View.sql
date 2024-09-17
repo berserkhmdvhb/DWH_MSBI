1022fc4cfb6b0e0c4e535d207de692ab038b87a6
Hamed <48640037+berserkhmdvhb@users.noreply.github.com>
2024-09-17 09:05:44 +0200

    Add files via upload

---
+++ Projects/CustomerData/Dataset/View.sql
@@ -0,0 +1,21 @@
+USE [CustomerDWH]
+GO
+
+/****** Object:  View [dbo].[V_FactCustomer]    Script Date: 5/14/2024 4:44:19 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+ALTER VIEW [dbo].[V_FactCustomer]
+AS
+SELECT        
+	RowNumber AS Idx,
+	CustomerCode AS [Code],
+	CustomerName AS [Name]
+FROM
+dbo.FactCustomer
+GO
+
+
