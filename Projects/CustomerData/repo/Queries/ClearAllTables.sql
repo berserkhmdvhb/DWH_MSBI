a0dc9ebbddb1934e538a6121fb3595e8c505ace0
Hamed <48640037+berserkhmdvhb@users.noreply.github.com>
2024-09-17 09:07:07 +0200

    Add files via upload

---
+++ Projects/CustomerData/repo/Queries/ClearAllTables.sql
@@ -0,0 +1,7 @@
+USE CustomerDWH;
+DELETE FROM dbo.DimCountry
+DELETE FROM dbo.DimProduct
+DELETE FROM dbo.DimSalesPerson
+DELETE FROM dbo.DimState
+TRUNCATE TABLE dbo.FactCustomer
+TRUNCATE TABLE dbo.FactCustomerTemp
