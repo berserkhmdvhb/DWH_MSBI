631221e9e46da1653f2f23fe6c0a78ac23452a3b
Hamed <48640037+berserkhmdvhb@users.noreply.github.com>
2024-09-24 12:56:39 +0200

    Delete Projects/CustomerData directory

--- Projects/CustomerData/repo/SSAS/Queries/DataSourceView_DimCountry.sql
+++
@@ -1,6 +0,0 @@
-SELECT 
-	[CountryID],
-	[CountryName],
-	[IsNew]
-FROM [CustomerDWH].[dbo].[DimCountry]
-WHERE IsNew = 1
--- Projects/CustomerData/repo/SSAS/Queries/DataSourceView_DimSalesPerson.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-	TD_SP_1.[SalesPersonID],
-	TD_SP_1.[SalesPersonName],
-	TD_SP_1.[SalesPersonBossID_FK],
-	TD_SP_2.[SalesPersonName] AS SalesPersonBossName 
-FROM [CustomerDWH].[dbo].[DimSalesPerson] AS TD_SP_1
-LEFT OUTER JOIN [CustomerDWH].[dbo].[DimSalesPerson] AS TD_SP_2
-	ON TD_SP_1.SalesPersonBossID_FK = TD_SP_2.SalesPersonID
