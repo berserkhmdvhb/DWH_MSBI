5eb1fccc11740996cdea0391aaac85907104b9d1
Hamed <48640037+berserkhmdvhb@users.noreply.github.com>
2024-09-25 17:38:14 +0200

    Add files via upload

---
+++ Projects/CustomerData/source/Queries/TableCreation_FactCustomer.sql
@@ -0,0 +1,56 @@
+USE [CustomerDWH]
+GO
+
+/****** Object:  Table [dbo].[FactCustomer]    Script Date: 5/3/2024 3:03:31 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[FactCustomer](
+	[RowNumber] [int] IDENTITY(1,1) NOT NULL,
+	[CustomerCode] [int] NOT NULL,
+	[CustomerName] [nvarchar](50) NULL,
+	[CustomerAmount] [money] NULL,
+	[SalesDate] [date] NULL,
+	[CountryID_FK] [int] NULL,
+	[StateID_FK] [int] NULL,
+	[ProductID_FK] [int] NULL,
+	[SalesPersonID_FK] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[CustomerCode] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[FactCustomer]  WITH NOCHECK ADD  CONSTRAINT [FK_FactCustomer_DimCountry] FOREIGN KEY([CountryId_FK])
+REFERENCES [dbo].[DimCountry] ([CountryID])
+GO
+
+ALTER TABLE [dbo].[FactCustomer] CHECK CONSTRAINT [FK_FactCustomer_DimCountry]
+GO
+
+ALTER TABLE [dbo].[FactCustomer]  WITH NOCHECK ADD  CONSTRAINT [FK_FactCustomer_DimProduct] FOREIGN KEY([ProductId_FK])
+REFERENCES [dbo].[DimProduct] ([ProductID])
+GO
+
+ALTER TABLE [dbo].[FactCustomer] CHECK CONSTRAINT [FK_FactCustomer_DimProduct]
+GO
+
+ALTER TABLE [dbo].[FactCustomer]  WITH NOCHECK ADD  CONSTRAINT [FK_FactCustomer_DimSalesPerson] FOREIGN KEY([SalesPersonId_FK])
+REFERENCES [dbo].[DimSalesPerson] ([SalesPersonID])
+GO
+
+ALTER TABLE [dbo].[FactCustomer] CHECK CONSTRAINT [FK_FactCustomer_DimSalesPerson]
+GO
+
+ALTER TABLE [dbo].[FactCustomer]  WITH NOCHECK ADD  CONSTRAINT [FK_FactCustomer_DimState] FOREIGN KEY([StateId_FK])
+REFERENCES [dbo].[DimState] ([StateID])
+GO
+
+ALTER TABLE [dbo].[FactCustomer] CHECK CONSTRAINT [FK_FactCustomer_DimState]
+GO
+
+
