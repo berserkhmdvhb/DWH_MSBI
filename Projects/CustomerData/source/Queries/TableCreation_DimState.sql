46dc543f81e4134135872d1f86c03f6e62ab2629
Hamed <48640037+berserkhmdvhb@users.noreply.github.com>
2024-09-25 17:59:52 +0200

    Add files via upload

---
+++ Projects/CustomerData/source/Queries/TableCreation_DimState.sql
@@ -0,0 +1,22 @@
+USE [CustomerDWH]
+GO
+
+/****** Object:  Table [dbo].[DimState]    Script Date: 5/20/2024 5:49:43 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[DimState](
+	[StateID] [int] NOT NULL,
+	[StateName] [nvarchar](50) NULL,
+	[CountryID] [int] NOT NULL
+ CONSTRAINT [PK_DimState] PRIMARY KEY CLUSTERED 
+(
+	[StateID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
