fd9328eae144f2e27173f388d40bf8032d239b61
Hamed <48640037+berserkhmdvhb@users.noreply.github.com>
2024-09-25 17:56:48 +0200

    Delete Projects/CustomerData/source/Queries directory

--- Projects/CustomerData/source/Queries/ClearAllTables.sql
+++
@@ -1,9 +0,0 @@
-USE CustomerDWH;
-DELETE FROM dbo.DimCountry
-DELETE FROM dbo.DimProduct
-DELETE FROM dbo.DimSalesPerson
-DELETE FROM dbo.DimState
-TRUNCATE TABLE dbo.FactCustomer
-TRUNCATE TABLE dbo.FactCustomerTemp
--TRUNCATE TABLE dbo.LogExec
---TRUNCATE TABLE dbo.DimDate
