79fe0fb29c52fb83dea6396157f1e82a5076229e
berserkhmdvhb <hamed.vaheb@gmail.com>
2024-12-07 09:27:30 +0100

    Finished STG schema

--- Projects/CustomerData/source/Queries/ClearAllTables.sql
+++ Projects/CustomerData/source/Queries/ClearAllTables.sql
@@ -26,7 +26,7 @@ DROP TABLE [LOG].[Tl_Exec]
 DROP TABLE [LOG].[Tl_Packages]
 */
 
-
+-- STG
 DELETE FROM stg.Td_Country
 DELETE FROM stg.Td_Product
 DELETE FROM stg.Td_SalesPerson
@@ -38,7 +38,7 @@ TRUNCATE TABLE stg.Td_SalesPerson
 TRUNCATE TABLE stg.Td_State
 
 
-
+-- DWH
 DELETE FROM dwh.Td_Country
 DELETE FROM dwh.Td_Product
 DELETE FROM dwh.Td_SalesPerson
@@ -47,12 +47,25 @@ DELETE FROM dwh.Tf_Customer
 TRUNCATE TABLE dwh.Tf_Customer
 
 
-DELETE FROM rep.FactCustomerTemp
-TRUNCATE TABLE rep.FactCustomerTemp
+-- LOG
+DELETE FROM [log].Tl_Exec
+TRUNCATE TABLE [log].Tl_Exec
+
+DELETE FROM [log].Tl_Packages
+ALTER TABLE [log].[Tl_Exec] DROP CONSTRAINT [FK_Tl_Exec_PackageId]
+TRUNCATE TABLE [log].Tl_Packages
+ALTER TABLE [log].[Tl_Exec] ADD CONSTRAINT [FK_Tl_Exec_PackageId] FOREIGN KEY ([PackageId]) REFERENCES [log].[Tl_Packages] ([PackageId])
 
 
-DELETE FROM [log].LogExec
-TRUNCATE TABLE [log].LogExec
+
+DELETE FROM [log].Tl_BusinessWarnings
+TRUNCATE TABLE [log].Tl_BusinessWarnings
+
+DELETE FROM [log].Tl_WarningDetails
+ALTER TABLE [log].[Tl_BusinessWarnings] DROP CONSTRAINT FK_BusinessWarnings_WarningDetails
+TRUNCATE TABLE [log].Tl_WarningDetails
+ALTER TABLE [log].[Tl_BusinessWarnings] ADD CONSTRAINT FK_BusinessWarnings_WarningDetails FOREIGN KEY (WarningID) REFERENCES [log].[Tl_WarningDetails] (WarningID) ON DELETE CASCADE
+
 
 --TRUNCATE TABLE dbo.DimDate
 
