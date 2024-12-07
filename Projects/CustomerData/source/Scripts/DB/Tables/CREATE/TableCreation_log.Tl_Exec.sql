084065c145c13a80061b61300ed3deeb31cefd75
berserkhmdvhb <hamed.vaheb@gmail.com>
2024-12-07 19:29:09 +0100

    fixed bug

--- Projects/CustomerData/source/Scripts/DB/Tables/CREATE/TableCreation_log.Tl_Exec.sql
+++ Projects/CustomerData/source/Scripts/DB/Tables/CREATE/TableCreation_log.Tl_Exec.sql
@@ -12,6 +12,7 @@ CREATE TABLE [log].[Tl_Exec] (
     [LogId] INT IDENTITY(1,1) NOT NULL, -- Auto-incrementing log ID
     [PackageId] UNIQUEIDENTIFIER NOT NULL, -- Links to [log].[Tl_Packages].[PackageId]
     [RunTime] DATETIME NOT NULL, -- Timestamp of the execution
+	[ExecutionStatus] NVARCHAR(50) NULL, -- Status: Success/Failure/Running
     CONSTRAINT [PK_Tl_Exec] PRIMARY KEY CLUSTERED ([LogId] ASC), -- Primary key
     CONSTRAINT [FK_Tl_Exec_PackageId] FOREIGN KEY ([PackageId]) -- Foreign key linking to Tl_Packages
         REFERENCES [log].[Tl_Packages] ([PackageId])
